ebaef193f2879a3ee78fdd5b227f21c88ff75838
bbenja <bbenja098@gmail.com>
2022-10-06 23:17:45 +0200

    added image to env

--- nfsug2.py
+++ nfsug2.py
@@ -16,6 +16,35 @@ import win32gui
 from random import choice
 import gym
 from gym.spaces import Discrete, Box
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras.optimizers import Adam
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy, LinearAnnealedPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+
+def build_model(w, h, actions):
+    model = Sequential()
+    model.add(Conv2D(32, (4, 4), strides=(2, 2),
+                     activation="relu", padding="same",
+                     input_shape=(w, h, 1)))
+    model.add(Flatten())
+    model.add(Dense(24, activation="relu"))
+    model.add(Dense(actions, activation="linear"))
+    return model
+
+
+def build_agent(model, actions):
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr="eps",
+                                  value_max=1., value_min=.1,
+                                  value_test=.2, nb_steps=10000)
+    memory = SequentialMemory(limit=1000, window_length=1)
+    dqn = DQNAgent(model=model, policy=policy, memory=memory,
+                   nb_actions=actions, nb_steps_warmup=10000)
+                   # enable_dueling_network=True, dueling_type="avg")
+    return dqn
+
 
 class CustomEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
@@ -23,33 +52,35 @@ class CustomEnv(gym.Env):
     def __init__(self):
         super(CustomEnv, self).__init__()
         self.action_space = Discrete(3)     #A, SPACE, D
-        self.observation_space = Box(0.0, 50.0, shape=(2,))
-
-        # self.states = 2
-        # self.actions = 3
+        self.observation_space = Box(0, 255, [800, 350, 1], dtype=np.uint8)
+        print(self.observation_space.shape)
+        self.state = get_car_diag()
+        print(self.state)
 
 
     def step(self, action):
-        # Apply action
         control_car(action)
+        self.state = get_car_diag()
 
-        # Calc reward
-        reward = get_score()
+        reward = 0
+        if self.state[1] != 0:
+            reward += int(self.state[1])
+        reward += get_score()
 
-        # Get state
-        state = get_car_diag()
-
-        # Calculate if done
         done = False
 
         info = {}
 
-        return state, reward, done, info
+        obs = np.reshape(get_car_from_image(get_window_image()), (350, 800, 1))
+        print(obs.shape)
+        #return self.state, reward, done, info
+        return obs, reward, done, info
 
     def reset(self):
-        state = get_car_diag()
         reset_race()
-        return state
+        obs = np.reshape(get_car_from_image(get_window_image()), (350, 800, 1))
+        print(obs.shape)
+        return obs
 
     def render(self):
         pass
@@ -58,126 +89,64 @@ class CustomEnv(gym.Env):
         pass
 
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.optimizers import Adam
-from rl.agents import DQNAgent
-from rl.policy import BoltzmannQPolicy
-from rl.memory import SequentialMemory
-
-def build_model(states, actions):
-    model = Sequential()
-    model.add(Flatten(input_shape=(1, states)))
-    model.add(Dense(24, activation="relu"))
-    model.add(Dense(24, activation="relu"))
-    model.add(Dense(actions, activation="linear"))
-    return model
-
-def build_agent(model, actions):
-    policy = BoltzmannQPolicy()
-    memory = SequentialMemory(limit=50000, window_length=1)
-    dqn = DQNAgent(model=model, memory=memory, policy=policy,
-                   nb_actions=actions, nb_steps_warmup=100, target_model_update=1e-2)
-    return dqn
-
-
-config = r"--psm 8 --oem 1"
-path = "C:\\Users\\bbenja\\Desktop\\nfsug.jpg"
-# W = 0x11; A = 0x1E; S = 0x1F;D = 0x20
+mem = Pymem("SPEED2.EXE")
+actions_array = [A, SPACE, D]
+keypress_pause = 0.3
+speed_offsets = [0x42C]
+angle_offsets = [0x214, 0x20, 0x394, 0xC8C, 0x4, 0x0, 0x6C]
+# config = r"--psm 8 --oem 1"
+# path = "C:\\Users\\bbenja\\Desktop\\nfsug.jpg"
 
 
 def get_window_image():
-    # window = pygetwindow.getWindowsWithTitle("NFS Underground 2")[0]
-    # left, top = window.topleft
-    # right, bottom = window.bottomright
-    # pyautogui.screenshot(path)
-    # im = Image.open(path)
-    # im = im.crop((left, top, right, bottom))
-
-    # im.save(path)
-    # im.show(path)
-
-    # im = ImageGrab.grab(bbox=(-7, 0, 807, 867))
     sct = mss()
-    im = np.array(sct.grab((-7, 0, 807, 867)))
-    cv2.imshow("frame", im)
-
-    return im
+    img = np.array(sct.grab((0, 100, 800, 820)))[:, :, :3]
+    return img
 
 
-def get_data_from_image(img):
-    # img = Image.open(path)
-    img = img.convert('L')
-    enhancer = ImageEnhance.Contrast(img)
-    img = enhancer.enhance(3)
-    img = img.filter(ImageFilter.MedianFilter())
-    cv2.imshow("image", cv2.resize(np.array(img), (500, 500)))
+def get_car_from_image(img):
+    img = img[160:, :, :]   # cut top part
+    img = img[:350, :, :]   # cut bottom part --> img = (350, 800, 4)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # img = (350, 800)
+    return img
 
 
-    crop_rectangle = (160, 230, 245, 260)
-    total = img.crop(crop_rectangle)   # (left, upper, right, lower)
-    # total = total.point(lambda x: 0 if x < 110 else 255)
-    cv2.imshow("total", np.array(total))
-    # total.show()
+# def get_data_from_image(img):
+#     img = img.convert('L')
+#     enhancer = ImageEnhance.Contrast(img)
+#     img = enhancer.enhance(3)
+#     img = img.filter(ImageFilter.MedianFilter())
+#     cv2.imshow("image", cv2.resize(np.array(img), (500, 500)))
+#
+#
+#     crop_rectangle = (160, 230, 245, 260)
+#     total = img.crop(crop_rectangle)   # (left, upper, right, lower)
+#     # total = total.point(lambda x: 0 if x < 110 else 255)
+#     cv2.imshow("total", np.array(total))
+#     # total.show()
+#
+#     crop_rectangle = (470, 785, 510, 825)
+#     angle = img.crop(crop_rectangle)
+#     # angle = angle.point(lambda x: 0 if x < 125 else 255)
+#     cv2.imshow("angle", np.array(angle))
+#     # angle.show()
+#
+#     detected_total = pytesseract.image_to_string(total, config=config)
+#     detected_angle = pytesseract.image_to_string(angle, config=config)
+#     detected_total = "".join(i for i in detected_total if i in "0123456789")
+#     detected_angle = sub("\D", "", detected_angle)
+#     return detected_total, detected_angle
 
-    crop_rectangle = (470, 785, 510, 825)
-    angle = img.crop(crop_rectangle)
-    # angle = angle.point(lambda x: 0 if x < 125 else 255)
-    cv2.imshow("angle", np.array(angle))
-    # angle.show()
 
-    detected_total = pytesseract.image_to_string(total, config=config)
-    detected_angle = pytesseract.image_to_string(angle, config=config)
-    detected_total = "".join(i for i in detected_total if i in "0123456789")
-    detected_angle = sub("\D", "", detected_angle)
-    return detected_total, detected_angle
 
-actions_array = [A, SPACE, D]
-actions_history = []
-keypress_pause = 0.3
-highscore, iteration, episode = 0, 0, 0
-states = 17
-active_state = 0
-Q = 1000 * np.random.rand(states, len(actions_array))
-lr = 0.5
-y = 0.5
 
 def control_car(chosen_action):
     for action in actions_array:
         ReleaseKey(action)
     PressKey(W)
     PressKey(actions_array[chosen_action])
-    print(actions_array[chosen_action])
+    print(print_action(actions_array[chosen_action]))
 
-
-def run_q_algorithm():
-    global active_state, highscore, episode, actions_history
-    chosen_action = np.argmax(Q[active_state, :])
-    actions_history.append(chosen_action)
-    print(f"state: {active_state}\tvalue: {Q[active_state, chosen_action]}\taction: {chosen_action}")
-
-    control_car(chosen_action)
-    reward = update_reward()
-    Q[active_state, chosen_action] = (1 - lr) * Q[active_state, chosen_action] + lr * (
-                highscore + y * np.max(Q[(active_state + 1), :]))
-    # print(Q)
-    if active_state + 2 != states:
-        active_state += 1
-    else:
-        reset_race()
-        episode += 1
-        print(f"EP: {episode}")
-        np.savetxt("qmatrix" + str(highscore) + ".out", Q, delimiter=',')
-        np.savetxt("actions_history" + str(highscore) + ".out", actions_history, delimiter=',')
-        active_state = 0
-        actions_history = []
-        highscore = 0
-
-def update_reward():
-    global highscore
-    highscore = mem.read_float((mem.base_address + 0x464650))
-    for i in range(len(actions_history)):
-        Q[i][actions_history[i]] += highscore
 
 def reset_race():
     #ESC, RIGHT, ENTER, LEFT, ENTER
@@ -212,76 +181,62 @@ def getPointerAddress(base, offsets):
         else:
             return remote_pointer.value + offset
 
-speed_offsets = [0x42C]
-angle_offsets = [0x214, 0x20, 0x394, 0xC8C, 0x4, 0x0, 0x6C]
 
 def get_car_diag():
     speed = mem.read_float(getPointerAddress(mem.base_address + 0x0049CCF8, speed_offsets))
     angle = mem.read_int(getPointerAddress(mem.base_address + 0x004B4754, angle_offsets))
-    return (speed, angle)
+    return speed, angle
 
 def get_score():
     total = mem.read_float((mem.base_address + 0x464650))
     return total
 
 def print_action(action):
-    bindings = {"W": 0x11, "A": 0x1E,
-                "S": 0x1F, "D": 0x20,
-                "ESC": 0x01, "ENTER": 0x1C,
-                "DOWN": 0xD0, "ONE": 0x02,
-                "CTRL": 0x1D, "SHIFT": 0x2A,
-                "SPACE": 0x39}
-    return list(bindings.keys())[list(bindings.values()).index(action)]
+    bindings = {"0x11": "W", "0x1e": "A",
+                "0x1f": "S", "0x20": "D",
+                "0x01": "ESC", "0x1c": "ENT",
+                "0xd0": "DOWN", "0x1d": "CTRL",
+                "0x2a": "SHIFT", "0x39": "SPACE"}
+    return bindings[str(hex(action))]
 
-mem = Pymem("SPEED2.EXE")
-# Instantiate the env
-env = CustomEnv()
+
 
 
 if __name__ == "__main__":
+    env = CustomEnv()
+    obs = env.reset()
+    height, width, ch = env.observation_space.shape
+    actions = env.action_space.n
     # window = win32gui.FindWindow(None, "NFS Underground 2")
     # win32gui.SetForegroundWindow(window)
-    print("The observation space: {}".format(env.observation_space))
-    for _ in range(5):
-        print(env.observation_space.sample())
 
 
-    model = build_model(2, 3)
-    # dqn = build_agent(model, 3)
-    # dqn.compile(Adam(1e-3), metrics=["mae"])
-    # dqn.fit(env, nb_steps=5000, visualize=False, verbose=1)
-    #
-    # dqn.save_weights("dqn", overwrite=True)
-    #
-    reset_race()
+    model = build_model(width, height, actions)
+    dqn = build_agent(model, actions)
+    dqn.compile(Adam(1e-4))
+
+    dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
+    dqn.save_weights("dqn", overwrite=True)
+
     model.load_weights("dqn")
-    for _ in range(50):
-        speed, angle = get_car_diag()
-        data = [speed, angle]
-        data = np.expand_dims(data, -1)
-        action = model.predict(data)
-        print(action)
+
+    dqn.test(env, nb_max_episode_steps=3000, visualize=False)
 
 
-    # for _ in range(1000):
-    #     action = env.action_space.sample()
-    #     data = env.step(action)
-    #     print(data)
 
     # while (True):
-    #     PressKey(W)
+    #     get_car_from_image(get_window_image())
+    #
     #     # get state
     #     state = get_car_diag()
     #     # predict based on the state
     #     action = choice(actions_array)
     #     # insert predicted action into the game
     #
-    #     sleep(keypress_pause)
-    #     ReleaseKey(action)
     #     # check the highscore and calculate the reward
     #     highscore = get_score()
     #     print(f"{round(state[0])}, {state[1]}\t-> {print_action(action)}\t-> {highscore}")
-
-        # key = cv2.waitKey(1)
-        # if key == 27:
-        #     break
+    #
+    #     key = cv2.waitKey(1)
+    #     if key == 27:
+    #         break
